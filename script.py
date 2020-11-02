38f544c01743e65d57b6fc171ab87d7d293d7ff2
tadeubanzato <tadeubanzato@gmail.com>
2020-11-02 16:38:19 -0500

    No changes except comments and documentation

--- script.py
+++ script.py
@@ -24,10 +24,14 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 # driver.get method() will navigate to a page given by the URL address
 driver.get('https://www.linkedin.com/login')
 
+# locate email form by element_by_id
 username = driver.find_element_by_id('username')
+# use data from Parameters file imported vars
 username.send_keys(parameters.linkedin_username)
 
+# locate email form by element_by_id
 password = driver.find_element_by_id('password')
+# use data from Parameters file imported vars
 password.send_keys(parameters.linkedin_password)
 
 # locate submit button by_class_name
@@ -67,7 +71,6 @@ with open(parameters.file_name, 'a') as f:
 
 # For loop to iterate over each URL in the list
     for linkedin_url in linkedin_urls:
-
         # open profile URL
         driver.get(linkedin_url)
 
@@ -79,8 +82,8 @@ with open(parameters.file_name, 'a') as f:
 
         name = sel.xpath('//*[starts-with(@class, "inline t-24 t-black t-normal break-words")]/text()').extract_first()
         if name:
-          name = name.strip()
-          # print(name)
+            name = name.strip()
+            # print(name)
 
         job_title = sel.xpath('//*[starts-with(@class, "mt1 t-18 t-black t-normal break-words")]/text()').extract_first()
         if job_title:
@@ -89,12 +92,12 @@ with open(parameters.file_name, 'a') as f:
 
         location = sel.xpath('//*[starts-with(@class, "t-16 t-black t-normal inline-block")]/text()').extract_first()
         if location:
-          location = location.strip()
-          # print(location)
-
-        linkedin_url = driver.current_url
+            location = location.strip()
+            # print(location)
 
         currentJob = sel.xpath('//h3/text()').extract_first()
+
+        linkedin_url = driver.current_url
 
         # validating if the fields exist on the profile
         name = validate_field(name)
@@ -102,7 +105,9 @@ with open(parameters.file_name, 'a') as f:
         location = validate_field(location)
         linkedin_url = validate_field(linkedin_url)
 
+        # Print what information is being write to the CSV file
         rows = [name,job_title,location,linkedin_url]
         print('Writing to CSV:', rows)
 
+        # Write ROW content to the CSV file
         csv_writer.writerow([name,job_title,location,linkedin_url])
